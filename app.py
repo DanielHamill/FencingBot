8b1e356d3c4ee202d05f81e971d2baff9acf9708
DanielHamill <DanielHamill00@gmail.com>
2020-01-06 18:56:21 -0500

    bot no longer replies to itself

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def webhook():
     log('Recieved {}'.format(data))
 
     # We don't want to reply to ourselves!
-    if data['name'] != 'apnorton-test-bot':
+    if data['name'] != 'daniel-bot-test':
         msg = '{}, you sent "{}".'.format(data['name'], data['text'])
         send_message(msg)
 
