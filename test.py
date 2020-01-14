235ce610c3461e1604a6da3604e3cca36638f3e6
DanielHamill <danielhamill@Daniels-MacBook-Air.local>
2020-01-13 19:51:14 -0500

    added help command

--- test.py
+++ test.py
@@ -1,12 +1,4 @@
-import sys
-import json
-
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
-
-from flask import Flask, request
-
-app = Flask(__name__)
+#hi ur cute
 
 commands = {
     '!Dues' : 'Dues are $60 a semster, $110 a year. Cash or venmo is accepted (venmo username here)',
@@ -14,7 +6,6 @@ commands = {
     '!UGAOpen' : 'Here is the askfred for the UGA Open: https://askfred.net/Events/moreInfo.php?tournament_id=45465'
 }
 
-# returns string with list of commands
 def printCommands():
     coms = 'Here are all my commands: '
     for state in commands:
@@ -36,36 +27,7 @@ def parseInput(msg):
             return 'no problem human, during the robot uprising I\'ll kill you last  : )'
     return ''
 
-# executes if app gets POST request. Parses and sends message
-@app.route('/', methods=['POST'])
-def webhook():
-    # gets data from post
-    data = request.get_json()
-    log('Recieved {}'.format(data))
+msg = '!Help'
 
-    # We don't want to reply to ourselves!
-    if data['name'] != 'Stabby':
-        input = data['text']
-        # parses commands from input
-        msg = parseInput(input)
-        if(msg != ''):
-            send_message(msg)
-
-    return "ok", 200
-
-# sends message in body of post to Group.me api
-def send_message(msg):
-    url  = 'https://api.groupme.com/v3/bots/post'
-
-    data = {
-          'bot_id' : '9441ec69d3223735bd84ef90eb',
-          'text'   : msg,
-         }
-
-    # sends post request
-    request = Request(url, urlencode(data).encode())
-    json = urlopen(request).read().decode()
-
-def log(msg):
-    print(str(msg))
-    sys.stdout.flush()
+out = parseInput(msg)
+print(out)
