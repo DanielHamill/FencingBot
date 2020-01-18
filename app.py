0c548cc702e81d6ed33beb295c296df4d2f591fd
DanielHamill <danielhamill@Daniels-MacBook-Air.local>
2020-01-18 17:30:14 -0500

    moved parseInput into a separate file for easier testing

--- app.py
+++ app.py
@@ -3,38 +3,11 @@ import json
 
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
-
 from flask import Flask, request
 
+import ParseInput as parse
+
 app = Flask(__name__)
-
-commands = {
-    '!Dues' : 'Dues are $60 a semster, $110 a year. Cash or venmo is accepted (venmo username here)',
-    '!TravelForm' : 'Here is the travel form: https://docs.google.com/forms/d/e/1FAIpQLSetkok6nL5vEQvBSvvAKLW5s0GpZm3Q4_B7vPy0OoCNKLutaA/viewform',
-    '!UGAOpen' : 'Here is the askfred for the UGA Open: https://askfred.net/Events/moreInfo.php?tournament_id=45465'
-}
-
-# returns string with list of commands
-def printCommands():
-    coms = 'Here are all my commands: '
-    for state in commands:
-        coms += '[' + state + '] '
-    return coms
-
-# parses input. If command is present in input, output the mapping of the command
-def parseInput(msg):
-    if(msg[0]=='!'):
-        #list commands
-        if(msg == '!Help'):
-            return printCommands()
-        #loops through the commands
-        for state in commands:
-            if(msg.find(state) != -1):
-                return commands[state]
-        #just an easter egg
-        if(msg.find('thank you Stabby') != -1 or msg.find('thanks Stabby') != -1):
-            return 'no problem human, during the robot uprising I\'ll kill you last  : )'
-    return ''
 
 # executes if app gets POST request. Parses and sends message
 @app.route('/', methods=['POST'])
@@ -47,7 +20,7 @@ def webhook():
     if data['name'] != 'Stabby':
         input = data['text']
         # parses commands from input
-        msg = parseInput(input)
+        msg = parse.parseInput(input)
         if(msg != ''):
             send_message(msg)
 
--- test.py
+++ test.py
@@ -1,33 +1,7 @@
-#hi ur cute
+import ParseInput as parse
 
-commands = {
-    '!Dues' : 'Dues are $60 a semster, $110 a year. Cash or venmo is accepted (venmo username here)',
-    '!TravelForm' : 'Here is the travel form: https://docs.google.com/forms/d/e/1FAIpQLSetkok6nL5vEQvBSvvAKLW5s0GpZm3Q4_B7vPy0OoCNKLutaA/viewform',
-    '!UGAOpen' : 'Here is the askfred for the UGA Open: https://askfred.net/Events/moreInfo.php?tournament_id=45465'
-}
+msg = "!ugaopen"
 
-def printCommands():
-    coms = 'Here are all my commands: '
-    for state in commands:
-        coms += '[' + state + '] '
-    return coms
+out = parse.parseInput(msg)
 
-# parses input. If command is present in input, output the mapping of the command
-def parseInput(msg):
-    if(msg[0]=='!'):
-        #list commands
-        if(msg == '!Help'):
-            return printCommands()
-        #loops through the commands
-        for state in commands:
-            if(msg.find(state) != -1):
-                return commands[state]
-        #just an easter egg
-        if(msg.find('thank you Stabby') != -1 or msg.find('thanks Stabby') != -1):
-            return 'no problem human, during the robot uprising I\'ll kill you last  : )'
-    return ''
-
-msg = '!Help'
-
-out = parseInput(msg)
 print(out)
